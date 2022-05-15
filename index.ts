9bd6d6fe00dcd5189cf8ed7987ceff79aeaa577e
Ranjeet <Ranjeet7494@outlook.com>
2022-05-15 14:31:37 +0530

    make3

--- index.ts
+++ index.ts
@@ -12,65 +12,64 @@ import { getAllTrashNotes, getSingleTrashNote, addNewTrashNote, deleteTrashNote,
 const prisma = new PrismaClient();
 
 const app = express();
-const router = express.Router();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser())
 
 //User- Management Routes
-router.post('/signup', register);
-router.post('/login', login);
+app.post('/signup', register);
+app.post('/login', login);
 //protected Route 
 //Refresh Access Token
-router.post('/refresh_token', authUtils, refreshToken)
+app.post('/refresh_token', authUtils, refreshToken)
 
 //Notes-Management Routes
 //Protected Routes
 //Route to show notes
-router.get('/all_notes', authUtils, getAllNote);
+app.get('/all_notes', authUtils, getAllNote);
 //route to add new note
-router.post('/add_new', authUtils, addNewNote);
+app.post('/add_new', authUtils, addNewNote);
 //route to show single note based on id of note
-router.get('/single_note:id', authUtils, getSingleNote);
+app.get('/single_note:id', authUtils, getSingleNote);
 //route to edit single note base on id of note
-router.post('/edit_note:id', authUtils, editExistingNote);
+app.post('/edit_note:id', authUtils, editExistingNote);
 //route to move a note to trash  by id
-router.post('/to_trash:id', authUtils, deleteNote);
+app.post('/to_trash:id', authUtils, deleteNote);
 
 
 //Archive Notes Management Routes
 //Protected Routes
 //Route to show archived notes
-router.get("/all_archived_notes", authUtils, getAllarchivedNotes);
+app.get("/all_archived_notes", authUtils, getAllarchivedNotes);
 //route to add new archived note
-router.post('/add_archive_note', authUtils, addNewArchivedNote);
+app.post('/add_archive_note', authUtils, addNewArchivedNote);
 //route to show single archived note based on id of note
-router.get('/single_archived_note:id', authUtils, getSinglearchivedNote);
+app.get('/single_archived_note:id', authUtils, getSinglearchivedNote);
 //route to edit single archived note base on id of note
-router.post('/edit_archived_note:id', authUtils, editExistingArchivedNote);
+app.post('/edit_archived_note:id', authUtils, editExistingArchivedNote);
 //route to move archived note to trash by id
-router.post('/to_trash_archive_note:id', authUtils, deleteArchivedNote);
+app.post('/to_trash_archive_note:id', authUtils, deleteArchivedNote);
 
 
 
 //Trash Notes Management Routes
 //Protected Routes
 //Route to show notes
-router.get("/all_trash_notes", authUtils, getAllTrashNotes);
+app.get("/all_trash_notes", authUtils, getAllTrashNotes);
 //route to add new note
-router.post('/add_trash_note', authUtils, addNewTrashNote);
+app.post('/add_trash_note', authUtils, addNewTrashNote);
 //route to show single note based on id of note
-router.get('/single_trash_note:id', authUtils, getSingleTrashNote);
+app.get('/single_trash_note:id', authUtils, getSingleTrashNote);
 //route to edit single note base on id of note
-router.post('/edit_trash_note:id', authUtils, editExistingTrashNote);
+app.post('/edit_trash_note:id', authUtils, editExistingTrashNote);
 //route to delete trash note by id
-router.post('/delete_trash_note:id', authUtils, deleteTrashNote);
+app.post('/delete_trash_note:id', authUtils, deleteTrashNote);
 //route to restore trash note
-router.post('/restore_trash_note:id', authUtils, restoreTrashNote);
+app.post('/restore_trash_note:id', authUtils, restoreTrashNote);
 
-app.use('/.netlify/functions/api', router)
 
-const server = app.listen(process.env.PORT, () =>
+
+const server = app.listen(process.env.PORT||PORT, () =>
     console.log(`🚀 Server ready on ${process.env.PORT}`)
 )
