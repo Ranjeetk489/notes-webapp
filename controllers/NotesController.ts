a9c8dc0216aa40486090bc45c638efd1bfad80e6
ranjeet <ranjeet7494@outlook.com>
2022-05-30 07:06:59 +0530

    brrr40

--- controllers/NotesController.ts
+++ controllers/NotesController.ts
@@ -90,6 +90,7 @@ async function editExistingNote(req: Request, res: Response) {
                 res.status(201).send({ success: updateNote })
         }
         catch (err) {
+                console.log(err)
                 res.status(500).json({ success: false })
         }
 }
